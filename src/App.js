6c1cc37ce29c2ed91521e2bd9191c80049f8ca3e
Marcelo Brito <marcellus.brito@gmail.com>
2023-09-07 23:56:19 -0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Iframe from 'react-iframe'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <Iframe url={process.env.REACT_APP_URL}
+        styles={{height: "100vh",
+        width: "100vw"
+      }}
+        id=""
+        className=""
+        display="block"
+        position="relative"/>
     </div>
   );
 }
