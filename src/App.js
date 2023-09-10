07334f53186b25da76b2d8ecf4b93b334913221d
Marcelo Brito <marcellus.brito@gmail.com>
2023-09-09 21:46:07 -0300

    Loader component and markers loop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 // import Iframe from 'react-iframe'
 // import LocationMarker from './components/LocationMarker';
+import { useState, useEffect } from 'react';
 import Map  from './components/Map'
+import Loader  from './components/Loader'
 
 function App() {
+  const [eventData, setEventData] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      setLoading(true)
+      const res = await fetch('https://eonet.gsfc.nasa.gov/api/v3/events')
+      const {events} = await res.json()
+
+      setEventData(events)
+      setLoading(false)
+    }
+
+    fetchEvents()
+  }, [])
+
   return (
     <div>
-      <Map />
+    { !loading ? <Map eventData={eventData} /> : <Loader /> }
       {/* <Iframe url={process.env.REACT_APP_URL}
         styles={{height: "100vh",
         width: "100vw"
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,20 +1,27 @@
 import GoogleMapReact from "google-map-react";
 import LocationMarker from "./LocationMarker";
 
-const Map = ({ center, zoom }) => {
+const Map = ({ eventData, center, zoom }) => {
+  const markers = eventData.map((ev, index) => {
+    if (ev.categories[0].id === "wildfires") {
+      return (
+        <LocationMarker
+          key={index}
+          lat={ev.geometry[0].coordinates[1]}
+          lng={ev.geometry[0].coordinates[0]}
+        />
+      );
+    }
+    return null;
+  });
   return (
     <div className="map">
       <GoogleMapReact
         bootstrapURLKeys={{ key: process.env.REACT_APP_KEY }}
-        defaultCenter={
-          (center = {
-            lat: 42.3265,
-            lng: -122.8756,
-          })
-        }
-        defaultZoom={6}
+        defaultCenter={center}
+        defaultZoom={zoom}
       >
-        <LocationMarker lat={center.lat} lng={center.lng} />
+        {markers}
       </GoogleMapReact>
     </div>
   );
